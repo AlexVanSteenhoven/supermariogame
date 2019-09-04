db908191df4b21e1d8f91193184aac1540f9f0d2
AlexvanSteenhoven <alex.steenhoven@gmail.com>
2019-09-04 21:56:12 +0200

    Refactored even more and added a timer to update the timing

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,39 @@
 import Compositor from './Compositor.js';
+import Timer from './Timer.js';
 import Entity from './Entity.js';
 import { loadLevel } from './loaders.js';
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
-import { createBackgroundLayer } from './layers.js';
+import { loadBackgroundSprites } from './sprites.js';
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
+import { createMario } from './entities.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(entity) {
-  return function drawSpriteLayer(context) {
-    entity.draw(context);
-  };
-}
+Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
+  ([mario, backgroundSprites, level]) => {
+    console.log('Level loader', level);
+    const comp = new Compositor();
 
-Promise.all([
-  loadMarioSprite(),
-  loadBackgroundSprites(),
-  loadLevel('1-1')
-]).then(([marioSprite, backgroundSprites, level]) => {
-  console.log('Level loader', level);
-  const comp = new Compositor();
+    const backgroundLayer = createBackgroundLayer(
+      level.backgrounds,
+      backgroundSprites
+    );
+    //comp.layers.push(backgroundLayer);
 
-  const backgroundLayer = createBackgroundLayer(
-    level.backgrounds,
-    backgroundSprites
-  );
-  comp.layers.push(backgroundLayer);
+    const gravity = 30;
+    mario.pos.set(64, 180);
+    mario.vel.set(200, -600);
 
-  const gravity = 0.5;
-  const mario = new Entity();
-  mario.pos.set(64, 180);
-  mario.vel.set(2, -10);
+    const spriteLayer = createSpriteLayer(mario);
+    comp.layers.push(spriteLayer);
 
-  mario.draw = function drawMario() {
-    marioSprite.draw('idle', context, this.pos.x, this.pos.y);
-  };
+    const timer = new Timer(1 / 60);
+    timer.update = function update(deltaTime) {
+      comp.draw(context);
+      mario.update(deltaTime);
+      mario.vel.y += gravity;
+    };
 
-  mario.update = function updateMario() {
-    this.pos.x += this.vel.x;
-    this.pos.y += this.vel.y;
-  };
-
-  comp.layers.push(createSpriteLayer(mario));
-
-  function update() {
-    comp.draw(context);
-    mario.update();
-    mario.vel.y += gravity;
-    requestAnimationFrame(update);
+    timer.start();
   }
-
-  update();
-});
+);
