8f375adf4c944da3660277823e90b27d0d43a4fc
AlexvanSteenhoven <alex.steenhoven@gmail.com>
2019-09-04 23:09:50 +0200

    Fixed bugs and added keyboard input

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,38 @@
 import Compositor from './Compositor.js';
 import Timer from './Timer.js';
-import Entity from './Entity.js';
 import { loadLevel } from './loaders.js';
+import { createMario } from './entities.js';
 import { loadBackgroundSprites } from './sprites.js';
 import { createBackgroundLayer, createSpriteLayer } from './layers.js';
-import { createMario } from './entities.js';
+
+import Keyboard from './KeyboardState.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
   ([mario, backgroundSprites, level]) => {
-    console.log('Level loader', level);
     const comp = new Compositor();
 
     const backgroundLayer = createBackgroundLayer(
       level.backgrounds,
       backgroundSprites
     );
-    //comp.layers.push(backgroundLayer);
+    comp.layers.push(backgroundLayer);
 
     const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+      if (keyState) {
+        mario.jump.start();
+      } else {
+        mario.jump.cancel();
+      }
+    });
+    input.listenTo(window);
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
@@ -30,7 +40,9 @@ Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
     const timer = new Timer(1 / 60);
     timer.update = function update(deltaTime) {
       mario.update(deltaTime);
+
       comp.draw(context);
+
       mario.vel.y += gravity * deltaTime;
     };
 
